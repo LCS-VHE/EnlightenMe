d55187cadac768e97ce252ecfe15eb789aef883c
Vincent He <vincent0710@Outlook.com>
2020-11-24 14:20:32 -0500

    Initial Commit

---
+++ EnlightenMe/EnlightenMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnlightenMeApp.swift
+//  EnlightenMe
+//
+//  Created by Vincent He on 2020-11-24.
+//
+
+import SwiftUI
+
+@main
+struct EnlightenMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
