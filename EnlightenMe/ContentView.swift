dc02e49d1abd81b55fe41db48d54a6ba706a4da4
Vincent He <vincent0710@Outlook.com>
2020-11-24 15:21:39 -0500

    Update layout

--- EnlightenMe/ContentView.swift
+++ EnlightenMe/ContentView.swift
@@ -9,33 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var projects = []
+    @State private var domain = ""
     
     var body: some View {
         NavigationView{
             VStack{
-                Text("Enlighten Me")
-                    .font(.body)
-                    .fontWeight(.ultraLight)
-                    .foregroundColor(Color.gray)
-                
-                Group{ // The group for the Enlighten Me
-                    VStack{
-                        HStack{
-                            Text("   About Projects: ") // The Space Bars are for the better looking view
-                            Spacer()
-                        }
-                        HStack{
+                ScrollView{
+                    Text("Enlighten Me")
+                        .font(.body)
+                        .fontWeight(.ultraLight)
+                        .foregroundColor(Color.gray)
+                    
+                    Group{ // The group for the Enlighten Me
+                        VStack{
                             HStack{
-                                ForEach(0..<10){index in
-                                    AboutProjectLinkView()
+                                Text("   About Projects: ") // The Space Bars are for the better looking view
+                                Spacer()
+                            }
+                            
+                            ScrollView(.horizontal, showsIndicators: false){
+                                HStack{ // The Navigation Link for About Project Page
+                                    HStack{
+                                        ForEach(0..<10){index in
+                                            NavigationLink(destination: Text("PlaceHolder") ){
+                                                AboutProjectLinkView()
+                                            }
+                                        }
+                                    }
                                 }
+                                
                             }
                         }
+                        
+                        
                     }
                 }
-                
-                
-                Spacer() 
+                Spacer()
                 HStack{ // Bottom Of the Screen Stuff
                     Text("Bottom Of the Screen Buttons View")
                 }
