c654675e17124117c843ec6e86511648bc1eb278
Vincent He <vincent0710@Outlook.com>
2020-11-24 14:48:10 -0500

    First Layout of the project

--- EnlightenMe/ContentView.swift
+++ EnlightenMe/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Text("Enlighten Me")
+                    .font(.body)
+                    .fontWeight(.ultraLight)
+                    .foregroundColor(Color.gray)
+                
+                Group{ // The group for the Enlighten Me
+                    VStack{
+                        HStack{
+                            Text("Title")
+                                .padding()
+                            Spacer()
+                        }
+                        HStack{
+                            HStack{
+                                Text("Navigation View")
+                                Text("Navigation View")
+                            }
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                HStack{ // Bottom Of the Screen Stuff
+                    Text("Bottom Of the Screen Buttons View")
+                }
+            }
+        }
+        
     }
 }
 
