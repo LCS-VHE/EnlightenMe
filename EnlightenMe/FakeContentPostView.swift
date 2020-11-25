66629bfa2544f6742620188e3a53a0767d854de3
Vincent He <vincent0710@Outlook.com>
2020-11-25 10:43:35 -0500

    The Post Gig Content View

---
+++ EnlightenMe/FakeContentPostView.swift
@@ -0,0 +1,45 @@
+//
+//  FakeContentPostView.swift
+//  EnlightenMe
+//
+//  Created by Vincent He on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentPostView: View {
+    @State private var ProfilePictures = "ProfilePicture Placeholder"
+    @State private var ProfileName = "PlaceHolder Name"
+    @State private var madeWith = "Neural Style Transfere"
+    @State private var Likes = 0
+    
+    var body: some View {
+        VStack{ // The Stack of the Image
+            HStack{
+                Image("ProfilePicture Placeholder").resizable().frame(width:45, height:45).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Text(" \(ProfileName)")
+                Spacer() // Pushing these to the side
+            }
+            
+            // THe post Image
+            Image("PostImage-PlaceHolder").resizable().frame(width:.infinity, height: 400)
+            
+            HStack{    // Images at the bottom to make it look good
+                Image("Like").resizable().frame(width:35, height: 35)
+                Image("Comment").resizable().frame(width:35, height: 35)
+                Image("Share").resizable().frame(width:35, height: 35)
+                Spacer()
+                
+                Text("Made With \(self.madeWith)")
+                    .multilineTextAlignment(.center)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct FakeContentPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentPostView()
+    }
+}
