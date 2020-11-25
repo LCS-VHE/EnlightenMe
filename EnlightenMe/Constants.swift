9612e3f0785863d04154ed69a18421ace84c157e
Vincent He <vincent0710@Outlook.com>
2020-11-25 11:15:30 -0500

    Constants

---
+++ EnlightenMe/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  EnlightenMe
+//
+//  Created by Vincent He on 2020-11-25.
+//
+
+import Foundation
+
+struct Constants{
+    var apiGateWay:String = ""
+    var domain:String = ""
+}
