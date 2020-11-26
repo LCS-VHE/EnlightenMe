966371bdcc02f2a2d8031f980a018340a720f996
Vincent He <vincent0710@Outlook.com>
2020-11-26 17:13:35 -0500

    Using tabview instead of navigationview

--- EnlightenMe/BottomButtonPickerView.swift
+++ EnlightenMe/BottomButtonPickerView.swift
@@ -1,38 +1,38 @@
 //
+
+
+
+//THIS IS A PLACE HOLDER FILE FOR TESTING 
+
+
+
 //  BottomButtonPickerView.swift
 //  EnlightenMe
 //
 //  Created by Vincent He on 2020-11-25.
 //
-
-import SwiftUI
-
-struct BottomButtonPickerView: View {
-    var body: some View {
-        HStack{
-            Spacer()
-            NavigationLink(destination: Text("PlaceHolder")){
-                Image("Home Logo").resizable().frame(width: 40, height:40)
-            }
-            
-            Spacer()
-            NavigationLink(destination: Text("PlaceHolder")){
-                Image("Instagram Logo").resizable().frame(width: 40, height:40)
-            }
-            
-            Spacer()
-            
-            NavigationLink(destination: ToolsNavigation()){
-                Image("Utilities Logo").resizable().frame(width:40, height: 40)
-            }
-            
-            Spacer()
-        }
-    }
-}
-
-struct BottomButtonPickerView_Previews: PreviewProvider {
-    static var previews: some View {
-        BottomButtonPickerView()
-    }
-}
+//
+//import SwiftUI
+//
+//struct BottomButtonPickerView: View {
+//    var body: some View {
+//        TabView{
+//            ContentView()
+//                .tabItem{
+//                    Image(systemName: "star.fill")
+//                    Text("Home Page")
+//                }
+//            ToolsNavigation()
+//                .tabItem{
+//                    Image(systemName: "star.fill")
+//                    Text("Utility")
+//                }
+//        }
+//    }
+//}
+//
+//struct BottomButtonPickerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BottomButtonPickerView()
+//    }
+//}
--- EnlightenMe/ContentView.swift
+++ EnlightenMe/ContentView.swift
@@ -8,63 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var aboutProjectLinkInfo:[AboutProjectLinkData] = getAboutProjectLinkData() // Data for the about project link view
-    private var domain:String = Constants().domain // The domain of the api
-    
     var body: some View {
-        NavigationView{
-            VStack{
-                ScrollView{
-//                    Text("Enlighten Me")
-//                        .font(.largeTitle)
-//                        .fontWeight(.ultraLight)
-//                        .foregroundColor(Color.gray)
-
-                    Group{ // The group for the Enlighten Me
-                        VStack{
-                            HStack{
-                                Text("   About Projects:") // The Space Bars are for the better looking view
-                                Spacer()
-                            }
-                            
-                            ScrollView(.horizontal, showsIndicators: false){ // The scroll View
-                                HStack{ // The Navigation Link for About Project Page
-                                    HStack{
-                                        ForEach(0..<aboutProjectLinkInfo.count){index in
-                                            NavigationLink(destination: Text("PlaceHolder") ){
-                                                AboutProjectLinkView(Data: aboutProjectLinkInfo[index])
-                                                
-                                            }
-                                        }
-                                    }
-                                }
-                                
-                            }
-                            
-                            
-                        }
-                    }
-                    
-                    Group{ // Trending I guess
-                        VStack{ // The content of the top pick
-                            HStack{ // Title
-                                Text("   Trending Pictures:")
-                                Spacer()
-                            }
-                            
-                            ForEach(0..<10){ index in // The Content Post View
-                                ContentPostView()
-                                Spacer()
-                            }
-                        }
-                    }
-                    
+        TabView{
+            HomePageView()
+                .tabItem{
+                    Image(systemName: "star.fill") // Place holder image
+                    Text("Home Page") // Title Home Pag
                 }
-                
-                Spacer()
-                BottomButtonPickerView() // The bottom of the project Page
-            }
-            .navigationBarTitle("Enlighten Me")
+            
         }
     }
 }
