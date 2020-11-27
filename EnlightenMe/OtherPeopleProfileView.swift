04e4e00fd6c0a41bfb8f52f9a654408423c9e4f3
Vincent He <vincent0710@Outlook.com>
2020-11-26 20:13:45 -0500

    Working on Other People profile

---
+++ EnlightenMe/OtherPeopleProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherPeopleProfileView.swift
+//  EnlightenMe
+//
+//  Created by Vincent He on 2020-11-26.
+//
+
+import SwiftUI
+
+struct OtherPeopleProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OtherPeopleProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherPeopleProfileView()
+    }
+}
