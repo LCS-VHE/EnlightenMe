8d9df5f1b036337de58ceb0e431a9dad452ba1c5
Vincent He <vincent0710@Outlook.com>
2020-11-27 22:04:36 -0500

    Added a new structure called AnimeFaceUploadData for uploading anime face data

--- EnlightenMe/DataStructures.swift
+++ EnlightenMe/DataStructures.swift
@@ -5,9 +5,17 @@
 //  Created by Vincent He on 2020-11-24.
 //
 
+import SwiftUI
+
 struct AboutProjectLinkData{
     var projectName: String
     var imageName: String
 }
 
-
+class AnimeFaceUploadData: ObservableObject{
+    @Published var captions = ""
+    @Published var title = ""
+    @Published var isPrivate = false
+    @Published var imageParms:[Double] = setupFaceCreatingUrlParms()
+    @Published var tags:[String] = ["", "", ""]
+}
