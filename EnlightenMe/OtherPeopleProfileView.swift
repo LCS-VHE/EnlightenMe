44101db35b5ce5fdaca3141b58e04572ffa715dc
Vincent He <vincent0710@Outlook.com>
2020-11-30 12:11:22 -0500

    Basic layout of other people profile view

--- EnlightenMe/OtherPeopleProfileView.swift
+++ EnlightenMe/OtherPeopleProfileView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct OtherPeopleProfileView: View {
+    @State private var userName = "Debug User"
+    @State private var Followers = 10
+    @State private var posts = 10
+    @State private var placeHolderValue = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ScrollView{
+                HStack{ // About Him Page
+                    VStack{
+                        Image("ProfilePicture-PlaceHolder").resizable().frame(width:75, height: 75).clipShape(Circle()).padding()
+                        Text("\(userName)")
+                            .multilineTextAlignment(.center)
+                    }
+                    
+                    Text("Follower\n\(Followers)")
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Text("Posts\n\(posts)")
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    
+                    Text("PlaceHolder\n\(placeHolderValue)")
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                }
+                Divider() // Dividing against user to post
+                
+                ForEach(0..<10){ num in
+                    SquareBoxPostView()
+                }
+            }
+                .navigationBarTitle(userName)
+        }
     }
 }
 
