ff9c05596d066fa2f593de95e8836abb6e0d198d
Vincent He <vincent0710@Outlook.com>
2020-12-05 23:15:41 -0500

    Changed domain for deployment

--- EnlightenMe/Constants.swift
+++ EnlightenMe/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants{
     var apiGateWay:String = ""
-    var domain:String = "http://127.0.0.1:5000"
+    var domain:String = "https://75f83b2572a9.ngrok.io"
 }
 
 struct ApiKey{
