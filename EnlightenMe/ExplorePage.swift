079152d706e85ff045109bef85acb67a24882aba
Vincent He <vincent0710@Outlook.com>
2020-12-06 10:51:43 -0500

    Fixed layout and added networking to grab data from endpoint

--- EnlightenMe/ExplorePage.swift
+++ EnlightenMe/ExplorePage.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
-struct ExplorePage: View {
+struct ExplorePage: View {        
+    @State private var trendingPost:AllContentPostViewData!
+    @State private var otherPost:AllContentPostViewData!
+    
+    @State private var sortedTrendingData = [[ContentPostViewData]]() // Post content view Data
+    @State private var sortedTrendingImageURL = [[String]]()
+    
+    @State private var sortedOtherPostData = [[ContentPostViewData]]() // Second page data
+    @State private var sortedOtherImageURL = [[String]]()
+
     var body: some View {
         NavigationView{
             ScrollView{
                 VStack{
-                    
+                    Divider()
                     HStack{ // Trending Title
                         Text("Trending")
                         Spacer()
@@ -26,23 +35,45 @@ struct ExplorePage: View {
                         }
                     }
                     
+                    
                     Divider()
+                    
                     HStack{ // Other Post
-                        Text("Other Posts").padding()
+                        Text("Other Posts")
                         Spacer()
                     }
                     Spacer()
                     
                     Group{ // Random Posts
-                        ForEach(0..<10){ num in
-                            SquareBoxPostView()
+                        ForEach(0..<sortedOtherPostData.count, id: \.self){ num in
+                            SquareBoxPostView(imageurl: sortedOtherImageURL[num], data: sortedOtherPostData[num])
                         }
                     }
                     
                 }
                 .navigationBarTitle("Explore")
             }
+        }.onAppear(perform: grab_data)
+    }
+    
+    func grab_data(){ // Getting the trending data and other data
+        guard let url = URL(string: "\(Constants().domain)/api/get-recent-posts") else {
+            return // Can't convert string to url
         }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in // Getting data to other posts
+            print("Hello")
+            if let data = data{
+                if let posts = try? JSONDecoder().decode(AllContentPostViewData.self, from: data){ // Data model, data input
+                    self.otherPost = posts
+                    self.sortedOtherImageURL = seperate_image_urls(data: otherPost)
+                    self.sortedOtherPostData = seperate_data_from_data(data: otherPost)
+                    
+                }
+            }
+            print("Hello")
+        }.resume()
+    
     }
 }
 
