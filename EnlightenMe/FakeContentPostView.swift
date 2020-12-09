7a982ac5ba890384b28b8547f46f90313dc3d6f8
Vincent He <vincent0710@Outlook.com>
2020-12-09 12:07:58 -0500

    SimpleContentView support Post

--- EnlightenMe/FakeContentPostView.swift
+++ EnlightenMe/FakeContentPostView.swift
@@ -6,30 +6,37 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct SimpleConetentPostView: View {
     @State private var ProfilePictures = "ProfilePicture Placeholder"
     @State private var ProfileName = "Mr.Random"
     
+    @State var data:[ContentPostViewData] = [ContentPostViewData]()
+    @State var imageURL:[String] = [String]()
+    
     var body: some View {
         VStack{ // The Stack of the Image
-            NavigationLink(destination: OtherPeopleProfileView() ){ // Going into other people page
-                HStack{
-                    Image("ProfilePicture Placeholder").resizable().frame(width:45, height:45).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    Text(" \(ProfileName)")
-                    Spacer() // Pushing these to the side
-                }
-            }
-            
-            NavigationLink(destination: PostContentView()){
-                // THe post Image
-                VStack{
-                    Image("PostImage-PlaceHolder")
-                        .resizable()
-                        .scaledToFit()
-                    
+            ForEach(0..<imageURL.count){ num in
+                
+                NavigationLink(destination: OtherPeopleProfileView() ){ // Going into other people page
+                    HStack{
+                        Image("ProfilePicture Placeholder").resizable().frame(width:45, height:45).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        Text(" \(ProfileName)")
+                        Spacer() // Pushing these to the side
+                    }
                 }
                 
+                NavigationLink(destination: PostContentView()){
+                    // THe post Image
+                    VStack{
+                        WebImage(url: URL(string: imageURL[num]))
+                            .resizable()
+                            .scaledToFit()
+                        
+                    }
+                    
+                }
             }
         }
     }
